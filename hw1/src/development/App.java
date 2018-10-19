e413d86b1dc2a0f5d05d76ed78b9db9b51561914
Furkan Emre <furkan54emre@gmail.com>
2018-10-19 22:24:16 +0300

    hw1 project created to show directory schema in github.

---
+++ hw1/src/development/App.java
@@ -0,0 +1,12 @@
+package development;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This project created to show hw1 project working environment");
+		System.out.println("Package name and class names has no meaning");
+
+	}
+
+}
