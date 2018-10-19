a2a0c8595fbc75c7bbe33e7bb4e3808e0135a6e7
Furkan Emre <furkan54emre@gmail.com>
2018-10-19 22:10:31 +0300

    Added App Class

---
+++ TestGit/src/development/App.java
@@ -0,0 +1,10 @@
+package development;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This project created to practice git and github integration");
+	}
+
+}
