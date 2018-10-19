a84b5605808fc68676a33c0e799c6b2e51e611bf
Furkan Emre <furkan54emre@gmail.com>
2018-10-19 22:13:00 +0300

    Person class added

---
+++ TestGit/src/development/Person.java
@@ -0,0 +1,16 @@
+package development;
+
+public class Person {
+	
+	private String name;
+	
+	public Person(String name) {
+		this.name = name;
+		
+	}
+	
+	public void printName() {
+		System.out.println(this.name);
+	}
+
+}
