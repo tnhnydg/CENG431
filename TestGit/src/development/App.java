5977701e515397791bf8454f69c6fbf4ea806273
Furkan Emre <furkan54emre@gmail.com>
2018-10-19 22:11:18 +0300

    Add print statement

--- TestGit/src/development/App.java
+++ TestGit/src/development/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This project created to practice git and github integration");
+		System.out.println("CENG431");
 	}
 
 }
