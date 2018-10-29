8b0cd0d3422ca856a05e066a3c7f56b233af7afa
Furkan Emre <furkan54emre@gmail.com>
2018-10-29 16:31:18 +0300

    Building ProjectMenu action schema

--- G11_CENG431_HW1/src/presentation/ProjectMenu.java
+++ G11_CENG431_HW1/src/presentation/ProjectMenu.java
@@ -15,8 +15,28 @@ public class ProjectMenu extends Menu {
 	}
 	
 	public void startManage() {
-		System.out.println("Project startManange...");
-		System.out.println(takeInput());
+		System.out.println("\t<<<Project Manage Menu>>>");
+		System.out.println("1-Add\n2-Remove\n3-Update\nq-to quit");
+		String input = takeInput();
+		
+		if(input.equals("1")) {
+			//add project
+			//addProject(pName, pDescription, pDate);
+		}
+		else if(input.equals("2")) {
+			//remove project
+			//removeProject(pName);
+		}
+		else if(input.equals("3")) {
+			//update project
+			//updateProject();
+		}
+		else if(input.toLowerCase().equals("q")) {
+			System.out.println("Quit is selected");
+		}
+		else {
+			System.out.println("Invalid");
+		}
 	}
 	
 	public void startDisplay() {
@@ -26,8 +46,49 @@ public class ProjectMenu extends Menu {
 	
 	public void startCalculate() {
 		System.out.println("Project startCalculate");
+		
 	}
-
-
-
+	
+	public void addProject(String pName, String pDescription, String pDate) {
+		//signature not final
+		//where we handle the date
+	}
+	
+	public void removeProject(String pName) {
+		//signature not final
+	}
+	
+	public void updateProject() {
+		//signature not final
+		//updateActivity();
+		//updateResource();
+	}
+	
+	public void updateActivity() {
+		//signature not final
+		//addActivity();
+		//removeActivity();
+		//??? I am not sure how to handle updating a activity option
+	}
+	
+	public void addActivity(String aDescription) {
+		//signature not final
+	}
+	
+	public void removeActivity(String aName) {
+		//signature not final
+	}
+	
+	public void removeTask(String tName) {
+		//signature not final
+	}
+	
+	public void updateResource() {
+		//signature not final
+	}
+	
+	public void addTask() {
+		//signature not final
+	}
+	
 }
