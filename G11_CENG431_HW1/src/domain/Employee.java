b6cd955c1cba820d4c0a94fdaeaf42ca010c5662
Furkan Emre <furkan54emre@gmail.com>
2018-10-31 22:25:21 +0300

    update employee and consultant

--- G11_CENG431_HW1/src/domain/Employee.java
+++ G11_CENG431_HW1/src/domain/Employee.java
@@ -1,12 +1,12 @@
 package domain;
 
+import java.util.Random;
 
 public class Employee extends Person {
 
 	public Employee(String eName, String eDescription) {
 		super(eName, eDescription);
-		//generateId
-		//setId
+		setId(generateId());
 	}
 	
 	public Employee(String eName, String eDescription, int rId) {
@@ -16,8 +16,16 @@ public class Employee extends Person {
 	
 	@Override
 	public int generateId() {
-		return 0;
+		Random r = new Random();
+		int low = 10000;
+		int high = 19999;
+		int result = r.nextInt(high-low) + low;
+		return result;
+	}
+	
+	@Override 
+	public String toString() {
+		return "Employee: " + super.toString();
 	}
 
-	
 }
