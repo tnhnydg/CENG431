f952d5d19b8d2969e16e3db1bdd9aaa1939703b4
Furkan Emre <furkan54emre@gmail.com>
2018-11-06 21:48:07 +0300

    Getter setter update

--- G11_CENG431_HW2/src/domain/Customer.java
+++ G11_CENG431_HW2/src/domain/Customer.java
@@ -14,4 +14,60 @@ public class Customer {
 		
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getSavings() {
+		return savings;
+	}
+
+	public void setSavings(double savings) {
+		this.savings = savings;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
--- G11_CENG431_HW2/src/domain/Order.java
+++ G11_CENG431_HW2/src/domain/Order.java
@@ -21,7 +21,7 @@ public class Order implements OrderState {
 	public Order(OrderState state) {
 		setOrderState(state);
 	}
-	
+		
 	public void setOrderState(OrderState state) {
 		this.state = state;
 	}
@@ -77,5 +77,93 @@ public class Order implements OrderState {
 	@Override
 	public void error() {
 		state.error();
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setTrackingNumber(int trackingNumber) {
+		this.trackingNumber = trackingNumber;
+	}
+	
+	public int getTrackingNumber() {
+		return this.trackingNumber;
+	}
+	
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	
+	public int getCustomerId() {
+		return this.customerId;
+	}
+	
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	public double getWeight() {
+		return this.weight;
+	}
+	
+	public void setShippingAddress(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+	
+	public String getShippingAddress() {
+		return this.shippingAddress;
+	}
+	
+	public void setDateShipped(LocalDate dateShipped) {
+		this.dateShippied = dateShipped;
+	}
+	
+	public LocalDate getDateShipped() {
+		return this.dateShippied;
+	}
+	
+	public void setDateDelivered(LocalDate dateDelivered) {
+		this.dateDelivered = dateDelivered;
+	}
+	
+	public LocalDate getDateDelivered() {
+		return this.dateDelivered;
+	}
+	
+	public double getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public double getCargoPrice() {
+		return cargoPrice;
+	}
+
+	public void setCargoPrice(double cargoPrice) {
+		this.cargoPrice = cargoPrice;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
 	}
 }
